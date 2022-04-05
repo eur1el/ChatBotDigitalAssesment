01f8c26f6cdc208b5ff047f4400d32f4ba9187fb
eur1el <17105@my.sanctamaria.school.nz>
2022-04-06 09:42:54 +1200

    pickupordeliveryv1

---
+++ informationv1.py
@@ -0,0 +1,14 @@
+
+#prints information request message
+print("Please enter your information")
+
+#asks for users name and saves as an input under "name"
+name = input("Please enter your name")
+
+#asks for users phone number an saves as input under "phone"
+phone = input("Please enter your phone number")
+
+#prints users input for "name"
+#prints users input for "phone"
+print(name)
+print(phone)
