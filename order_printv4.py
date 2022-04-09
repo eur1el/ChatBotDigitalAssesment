13c5c79d550c93e17cfc9efe6755c9edc0fa472b
eur1el <99291835+eur1el@users.noreply.github.com>
2022-04-10 02:22:07 +1200

    Created Print_Order_Function
    Added formating to print_order and added total cost. Developed into a afunction

---
+++ order_printv4.py
@@ -0,0 +1,22 @@
+#list to store ordered mousepads
+order_list=['Wave Design- Small','Sakura blossom Design - Small','Plain Black Design - Small']
+#list to store mousepad prices
+order_cost = [12.50, 12.50, 12.50]
+
+cust_details = {'name':'Euriel','phone':'0212121213','house':'45','street':'Daniel','subrb':'Mangere'}
+
+def print_order():
+    total_cost = sum(order_cost)
+    print ("Customer Details")
+    print(f"{cust_details['name']} {cust_details['phone']} {cust_details['house']} {cust_details['street']} {cust_details['suburb']}")
+    print()
+    print("Order Details")
+    count = 0
+    for order in order_list:
+        print("Ordered: {}  Cost ${:.2f}".format(item,order_cost[count]))
+        count = count+1
+    print()
+    print("Order Cost Details")
+    print(f"${total_cost:.2f}")
+
+print_order()
