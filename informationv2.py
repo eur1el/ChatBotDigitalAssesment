28d5d62b72121aa2a65885ce506b30671ae7fead
eur1el <99291835+eur1el@users.noreply.github.com>
2022-04-09 18:27:02 +1200

    added customer dictionary

--- informationv2.py
+++ informationv2.py
@@ -1,3 +1,6 @@
+#customer details dictionary
+customer_details = {}
+
 
 #prints information request message
 print("Please enter your information")
@@ -5,22 +8,23 @@ print("Please enter your information")
 #asks for users name and saves as an input under "name"
 valid = False
 while not valid:
-    name = input("Please enter your name")
-    if name != "":
-        print(name)
+    customer_details['name'] = input("Please enter your name")
+    if customer_details['name'] != "":
+        print (customer_details['name']
+        break
+    else:
+        print("Sorry this cannot be blank")
+
+#asks for users phone number an saves as input under "phone"
+valid = False
+while not valid:
+    customer_details['name'] = input("Please enter your phone number")
+    if customer_details['phone'] != "":
+        print (customer_details['phone']
         break
     else:
         print("Sorry this number cannot be blank")
 
-#asks for users phone number an saves as input under "phone"
-valid= False
-while not valid:
-    phone = input("Please enter your phone number")
-    if phone != "":
-        print(phone)
-        break
-    else:
-        print("Sorry this cannot be blank")
 
 
 #prints users input for "name"
